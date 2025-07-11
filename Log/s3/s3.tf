6d4e408ccef9885518291b680054ed89066b6e46
BISHOP1027 <163997602+BISHOP1027@users.noreply.github.com>
2025-07-12 00:19:10 +0900

    Update s3.tf

--- Log/s3/s3.tf
+++ Log/s3/s3.tf
@@ -1,1 +1,16 @@
 #s3.tf
+
+# VPC FLOW LOG 버킷
+resource "aws_s3_bucket" "vpc_flow_log_bucket" {
+  bucket = "logrrr-vpc-flow-log-bucket"
+}
+
+# AWS GAURDDUTY 버킷
+resource "aws_s3_bucket" "guardduty_result_bucket" {
+  bucket = "logrrr-gaurdduty-result-bucket"
+}
+
+# AWS CLOUDTRAIL 버킷
+resource "aws_s3_bucket" "cloudtrail_log_bucket" {
+  bucket = "logrrr-cloudtrail-log-bucket"
+}
