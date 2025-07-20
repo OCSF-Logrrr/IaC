25a418fabfb86a40dd424fb1a55fcf6b54531675
BISHOP1027 <jy10270@naver.com>
2025-07-20 17:52:37 +0900

    설명: 주석 정리 완료

--- Log/filebeat_ec2/filebeat_ec2.tf
+++ Log/filebeat_ec2/filebeat_ec2.tf
@@ -1,40 +1,49 @@
 #filebeat_ec2.tf
 
+##############################################
+# Filebeat 수집기 EC2 인스턴스 구성
+##############################################
+
 resource "aws_instance" "filebeat" {
-  ami                    = "ami-0d5bb3742db8fc264"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.filebeat_public_subnet.id
-  vpc_security_group_ids = [aws_security_group.filebeat_sg.id]
-  key_name               = var.key_name
+  ami                    = "ami-0d5bb3742db8fc264"  # Ubuntu 또는 Filebeat 설치용 베이스 AMI ID
+  instance_type          = "t2.micro"               # 테스트 용도로 사용되는 최소 사양 인스턴스
+  subnet_id              = aws_subnet.filebeat_public_subnet.id  # 퍼블릭 서브넷에 배치
+  vpc_security_group_ids = [aws_security_group.filebeat_sg.id]   # Filebeat용 보안그룹 적용
+  key_name               = var.key_name             # SSH 접속용 키 페어 이름
 
-  iam_instance_profile = aws_iam_instance_profile.filebeat_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.filebeat_profile.name  # S3, SQS 접근을 위한 IAM 역할 연결
 
-  # user_data.tpl 파일을 EC2에 전달
+  # EC2 초기 실행 시 실행될 셸 스크립트(user_data) 전달 (Filebeat 설치 및 설정)
   user_data = templatefile("${path.module}/user_data.tpl", {
-    cloudtrail_event_queue_url              = var.cloudtrail_sqs_queue_url
-    guardduty_event_queue_url               = var.guardduty_sqs_queue_url
-    vpc_flow_event_queue_url                = var.vpc_flow_sqs_queue_url
-    cloudtrail_s3_bucket_id   = var.cloudtrail_s3_bucket_id
-    cloudtrail_s3_bucket_arn = var.cloudtrail_s3_bucket_arn
-    guardduty_s3_bucket_id    = var.guardduty_s3_bucket_id
-    guardduty_s3_bucket_arn = var.guardduty_s3_bucket_arn
-    vpc_flow_log_s3_bucket_id = var.vpc_flow_log_s3_bucket_id
-    vpc_flow_log_s3_bucket_arn = var.vpc_flow_log_s3_bucket_arn
+    cloudtrail_event_queue_url    = var.cloudtrail_sqs_queue_url   # CloudTrail SQS 큐 URL
+    guardduty_event_queue_url     = var.guardduty_sqs_queue_url    # GuardDuty SQS 큐 URL
+    vpc_flow_event_queue_url      = var.vpc_flow_sqs_queue_url     # VPC Flow Logs SQS 큐 URL
+
+    cloudtrail_s3_bucket_id       = var.cloudtrail_s3_bucket_id    # CloudTrail 로그 S3 버킷 ID
+    cloudtrail_s3_bucket_arn      = var.cloudtrail_s3_bucket_arn   # CloudTrail 로그 S3 버킷 ARN
+
+    guardduty_s3_bucket_id        = var.guardduty_s3_bucket_id     # GuardDuty 로그 S3 버킷 ID
+    guardduty_s3_bucket_arn       = var.guardduty_s3_bucket_arn    # GuardDuty 로그 S3 버킷 ARN
+
+    vpc_flow_log_s3_bucket_id     = var.vpc_flow_log_s3_bucket_id  # VPC Flow 로그 S3 버킷 ID
+    vpc_flow_log_s3_bucket_arn    = var.vpc_flow_log_s3_bucket_arn # VPC Flow 로그 S3 버킷 ARN
   })
 
   tags = {
-    Name = "filebeat-instance"
+    Name = "filebeat-instance"   # EC2 식별용 태그
     type = "ec2_instance"
   }
 }
 
+##############################################
+# EC2에 고정 퍼블릭 IP(EIP) 할당
+##############################################
 
-#eip 생성
 resource "aws_eip" "filebeat_eip" {
-  instance = aws_instance.filebeat.id
+  instance = aws_instance.filebeat.id  # 위에서 만든 filebeat EC2에 EIP 연결
 
   tags = {
-    Name = "filebeat-eip"
+    Name = "filebeat-eip"  # EIP 리소스 식별용 태그
     type = "eip"
   }
 }
--- Log/filebeat_ec2/variables.tf
+++ Log/filebeat_ec2/variables.tf
@@ -1,104 +1,121 @@
 #variables.tf
 
 ###########################################################################################
-########################################## VPC ############################################
+########################################## VPC ###########################################
 ###########################################################################################
 
+# 전체 VPC의 CIDR 블록
 variable "vpc_cidr" {
-  type = string
-  default = "20.0.0.0/16"
-  description = "VPC CIDR"
+  type        = string
+  default     = "20.0.0.0/16"
+  description = "CIDR block for the VPC"
 }
 
 ###########################################################################################
-####################################### Subnet ############################################
+####################################### Subnet ###########################################
 ###########################################################################################
 
+# 퍼블릭 서브넷의 CIDR 블록
 variable "public_subnet_cidr" {
-  type = string
-  default = "20.0.1.0/24"
-  description = "Public subnet CIDR"
+  type        = string
+  default     = "20.0.1.0/24"
+  description = "CIDR block for the public subnet"
 }
 
+# 퍼블릭 서브넷이 배치될 가용 영역
 variable "availability_zone" {
-  type = string
-  default = "ap-northeast-2a"
+  type        = string
+  default     = "ap-northeast-2a"
   description = "Availability Zone for the public subnet"
 }
 
 ###########################################################################################
-########################################## Key ##############################################
+########################################## SSH Key #######################################
 ###########################################################################################
 
+# EC2 인스턴스 접속에 사용할 키페어 이름
 variable "key_name" {
-  type = string
+  type        = string
+  default     = "whs_ocsf_logrrr"
   description = "Name of the key pair to use for SSH access"
-  default = "whs_ocsf_logrrr"
 }
 
 ###########################################################################################
-########################################## SQS ##############################################
+########################################## SQS ###########################################
 ###########################################################################################
 
+# CloudTrail 이벤트를 수신하는 SQS 큐 ARN (IAM 정책에 사용)
 variable "cloudtrail_sqs_queue_arn" {
-  type = string
-  description = "SQS queue ARN for receiving CloudTrail events"
+  type        = string
+  description = "ARN of the SQS queue receiving CloudTrail events"
 }
 
+# GuardDuty 이벤트를 수신하는 SQS 큐 ARN
 variable "guardduty_sqs_queue_arn" {
-  type = string
-  description = "SQS queue ARN for receiving GuardDuty events"
+  type        = string
+  description = "ARN of the SQS queue receiving GuardDuty events"
 }
 
+# VPC Flow 로그를 수신하는 SQS 큐 ARN
 variable "vpc_flow_sqs_queue_arn" {
-  type = string
-  description = "SQS queue ARN for receiving VPC Flow events"
+  type        = string
+  description = "ARN of the SQS queue receiving VPC Flow logs"
 }
 
+# CloudTrail 이벤트용 SQS 큐 URL (Filebeat 설정에서 사용)
 variable "cloudtrail_sqs_queue_url" {
-  type = string
-  description = "SQS queue URL for receiving CloudTrail events"
+  type        = string
+  description = "URL of the SQS queue receiving CloudTrail events"
 }
 
+# GuardDuty 이벤트용 SQS 큐 URL
 variable "guardduty_sqs_queue_url" {
-  type = string
-  description = "SQS queue URL for receiving GuardDuty events"
+  type        = string
+  description = "URL of the SQS queue receiving GuardDuty events"
 }
 
+# VPC Flow 로그용 SQS 큐 URL
 variable "vpc_flow_sqs_queue_url" {
-  type = string
-  description = "SQS queue URL for receiving VPC Flow events"
+  type        = string
+  description = "URL of the SQS queue receiving VPC Flow logs"
 }
+
 ###########################################################################################
-########################################## S3 ##############################################
+########################################## S3 ###########################################
 ###########################################################################################
 
+# CloudTrail 로그가 저장될 S3 버킷 ARN (IAM 정책에 사용)
 variable "cloudtrail_s3_bucket_arn" {
-  type = string
-  description = "S3 bucket ARN for CloudTrail logs"
+  type        = string
+  description = "ARN of the S3 bucket storing CloudTrail logs"
 }
 
+# CloudTrail 로그 S3 버킷 ID (리소스 ID 또는 이름)
 variable "cloudtrail_s3_bucket_id" {
-  type = string
-  description = "S3 bucket ID for CloudTrail logs"
+  type        = string
+  description = "ID of the S3 bucket storing CloudTrail logs"
 }
 
+# GuardDuty 결과 저장용 S3 버킷 ARN
 variable "guardduty_s3_bucket_arn" {
-  type = string
-  description = "S3 bucket ARN for GuardDuty findings"
+  type        = string
+  description = "ARN of the S3 bucket storing GuardDuty findings"
 }
 
+# GuardDuty 결과 S3 버킷 ID
 variable "guardduty_s3_bucket_id" {
-  type = string
-  description = "S3 bucket ID for GuardDuty findings"
+  type        = string
+  description = "ID of the S3 bucket storing GuardDuty findings"
 }
 
+# VPC Flow 로그 저장용 S3 버킷 ARN
 variable "vpc_flow_log_s3_bucket_arn" {
-  type = string
-  description = "S3 bucket ARN for VPC Flow Logs"
+  type        = string
+  description = "ARN of the S3 bucket storing VPC Flow logs"
 }
 
+# VPC Flow 로그 S3 버킷 ID
 variable "vpc_flow_log_s3_bucket_id" {
-  type = string
-  description = "S3 bucket ID for VPC Flow Logs"
+  type        = string
+  description = "ID of the S3 bucket storing VPC Flow logs"
 }
